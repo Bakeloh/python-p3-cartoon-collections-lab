ec4fd08ef271da20f1b09ee5eb449085568e6b9a
BakelohNoel <bakelohnoel@gmail.com>
2024-02-16 10:44:21 +0300

    Completed Assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,16 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, 1):
+        print(f"{i}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(elements):
+    return [element.capitalize() + '!' for element in elements]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(items):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in items:
+        if item in cheeses:
+            return item
+    return None
